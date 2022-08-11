182c391ff0737cc3b98b0b2bfa58d89ee2335f2b
cacce98 <carol98andres@gmail.com>
2022-08-10 19:44:26 -0500

    Agregando proyecto Farmalvergue

---
+++ Alvergue/registros/forms.py
@@ -0,0 +1,26 @@
+
+from django import forms
+from .models import Archivos, SalidaMedicamentos
+from django.forms import ClearableFileInput, ModelForm
+
+class SalidaForm(ModelForm):
+    class Meta:
+        model = SalidaMedicamentos
+        fields = ['AsignadoA', 'Fecha', 'Medicamento', 'Cantidad', 'archivo']
+
+       
+
+class CustomClearableFieldInput(ClearableFileInput):
+    template_with_clear = '<br> <label for="%(clear_checkbox_id)s">%(clear_checkbox_label)s</label> %(clear)s'
+
+class FormArchivos(ModelForm):
+    class Meta:
+        model = Archivos
+        fields = ('NombrePaciente', 'Edad', 'Sexo', 'CURP', 'Direccion', 'Telefono','Medicamento' ,'archivo')
+        widgets = {
+            'archivo': CustomClearableFieldInput
+        }
+class FormEditar(forms.ModelForm):
+    class Meta:
+        model = Archivos
+        fields = ['NombrePaciente', 'Edad', 'Sexo', 'CURP', 'Direccion', 'Telefono','Medicamento' ,'archivo']
