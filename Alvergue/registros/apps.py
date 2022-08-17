e50793e290dc84619eaf0aa6a11bc69c25030f52
cacce98 <107410653+cacce98@users.noreply.github.com>
2022-08-16 21:47:56 -0500

    Delete Alvergue/registros directory

--- Alvergue/registros/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegistrosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'registros'
-    verbose_name: str = 'Registro de Medicamentos'
